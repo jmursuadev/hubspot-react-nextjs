0874e910a28d9d7b5378f4e530bbdf50107375a6
Jonathan Ursua <37826165+jmursuadev@users.noreply.github.com>
2024-06-05 12:27:26 +0800

    Update Config.php

--- webapp/backend/src/Config/Config.php
+++ webapp/backend/src/Config/Config.php
@@ -4,8 +4,8 @@ namespace Sona\Backend\Config;
 
 class Config
 {
-    public static $clientId = 'e817f120-9dcf-4a60-8b56-ccba0242fc6f';
-    public static $clientSecret = 'c576417f-1c6e-4add-810d-61243904234c';
+    public static $clientId = '';
+    public static $clientSecret = '';
     public static $redirectUri = 'http://localhost:8080/oauth/callback';
     public static $scope = 'crm.objects.contacts.read';
     public static $frontendUrl = 'http://localhost:3000';
